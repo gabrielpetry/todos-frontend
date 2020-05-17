c2b8c1590a760539aceae51bb49b352c37f58f60
Gabriel Petry <contato@gabrilpetry.com.br>
2020-05-17 15:57:49 -0300

    changed from _id to id

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -20,8 +20,8 @@ export default function TodoList() {
   };
 
   const loadBookTasks = async (book) => {
-    if (!book._id) return;
-    const res = await api.getBookTasks(book._id);
+    if (!book.id) return;
+    const res = await api.getBookTasks(book.id);
     setBookTasks(res.data.tasks);
   };
 
@@ -39,11 +39,11 @@ export default function TodoList() {
 
   const toggleTask = useCallback(
     (task) => (event) => {
-      api.updateOne(book._id, task).then((res) => {
+      api.updateOne(book.id, task).then((res) => {
         if (res.status !== 200) return;
         // if update succeses locally update the list
         const newTodos = bookTasks.map((t) => {
-          if (t._id === task._id) {
+          if (t.id === task.id) {
             t.completed = !t.completed;
           }
           return t;
@@ -51,7 +51,7 @@ export default function TodoList() {
         setBookTasks(newTodos);
       });
     },
-    [book._id, bookTasks]
+    [book.id, bookTasks]
   );
 
   const addBook = useCallback((event) => {
@@ -66,21 +66,19 @@ export default function TodoList() {
   const addTask = useCallback(
     (event) => {
       const description = window.prompt("What you have to do?");
-      api
-        .createTask(book._id, { description, completed: false })
-        .then((res) => {
-          if (res.status !== 201) return;
-          loadBookTasks(book);
-        });
+      api.createTask(book.id, { description, completed: false }).then((res) => {
+        if (res.status !== 201) return;
+        loadBookTasks(book);
+      });
     },
     [book]
   );
 
   const removeTask = useCallback(
-    (book_id, task_id) => (event) => {
-      api.deleteTask(book_id, task_id).then((res) => {
+    (bookid, taskid) => (event) => {
+      api.deleteTask(bookid, taskid).then((res) => {
         if (res.status !== 200) return;
-        const newTodos = bookTasks.filter((t) => t._id !== task_id);
+        const newTodos = bookTasks.filter((t) => t.id !== taskid);
         setBookTasks(newTodos);
       });
     },
@@ -88,10 +86,10 @@ export default function TodoList() {
   );
 
   const removeBook = useCallback(
-    (book_id) => (event) => {
-      api.deleteBook(book_id).then((res) => {
+    (bookid) => (event) => {
+      api.deleteBook(bookid).then((res) => {
         if (res.status !== 204) return;
-        const newBooks = books.filter((t) => t._id !== book_id);
+        const newBooks = books.filter((t) => t.id !== bookid);
         setBooks(newBooks);
         setBook(books[0]);
       });
@@ -105,7 +103,7 @@ export default function TodoList() {
         <h1>{book.name}</h1>
         <div className="actions">
           <FontAwesomeIcon
-            onClick={removeBook(book._id)}
+            onClick={removeBook(book.id)}
             className="remove-book"
             icon={faTimesCircle}
           />
@@ -116,9 +114,9 @@ export default function TodoList() {
         {books.length &&
           books.map((b) => (
             <span
-              key={b._id}
+              key={b.id}
               onClick={() => setBook(b)}
-              className={b._id === book._id ? "book active" : "book"}
+              className={b.id === book.id ? "book active" : "book"}
             >
               {b.name}
             </span>
@@ -133,7 +131,7 @@ export default function TodoList() {
         {bookTasks.length &&
           bookTasks.map((task) => (
             <li
-              key={task._id}
+              key={task.id}
               className={task.completed ? "completed" : "notCompleted"}
             >
               <div className="task-inline">
@@ -148,7 +146,7 @@ export default function TodoList() {
                   <label>{task.description}</label>
                 </div>
                 <FontAwesomeIcon
-                  onClick={removeTask(book._id, task._id)}
+                  onClick={removeTask(book.id, task.id)}
                   className="remove-book"
                   icon={faTimesCircle}
                 />
